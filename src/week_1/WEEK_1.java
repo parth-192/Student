6a3512ff94911755074e4521c241226cf85514d4
parth-192 <50718460+parth-192@users.noreply.github.com>
2019-05-23 16:00:06 -0400

    Update WEEK_1.java

--- src/week_1/WEEK_1.java
+++ src/week_1/WEEK_1.java
@@ -18,6 +18,7 @@ public class WEEK_1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello!!");
+     System.out.println("fuck off...");
     }
     
 }
